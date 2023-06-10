0abff79a68affabfc0b14782905c48528043f95d
littleAndroidMan <richard.stannard@live.com>
2023-06-10 10:12:45 -0500

    Moved TestModel2 into TestModel folder.

--- test/EventUITestFramework/TestModel/Enums.cs
+++ test/EventUITestFramework/TestModel/Enums.cs
@@ -11,12 +11,54 @@ using System.Threading.Tasks;
 
 namespace EventUITestFramework.TestModel
 {
-    public enum EventUITestItemType
+
+    /// <summary>
+    /// Indicates what type of test "runnable" object is being referenced.
+    /// </summary>
+    public enum TestRunnableType
     {
+        /// <summary>
+        /// Default.
+        /// </summary>
         None = 0,
-        Test = 1,
-        TestFile = 2,
-        TestSet = 3,
-        TestRoot = 4,
+        /// <summary>
+        /// Item is a dependency for a test.
+        /// </summary>
+        Dependency = 1,
+        /// <summary>
+        /// Item is a set of Runnables.
+        /// </summary>
+        Set = 2,
+        /// <summary>
+        /// Item is a code file with JavaScript tests inside.
+        /// </summary>
+        File = 3,
+        /// <summary>
+        /// Item is a piece of JavaScript test code.
+        /// </summary>
+        TestCode = 4,
+        /// <summary>
+        /// Item is a test root and starts a new hierarchy.
+        /// </summary>
+        Root = 5
+    }
+
+    /// <summary>
+    /// Modes for indicating what should happen should a runnable fail.
+    /// </summary>
+    public enum TestFailureMode
+    {
+        /// <summary>
+        /// A failing test will not stop any further execution of tests.
+        /// </summary>
+        Continue = 0,
+        /// <summary>
+        /// A failing test will stop the execution of the container that contains it.
+        /// </summary>
+        Abandon = 1,
+        /// <summary>
+        /// A failing test will stop all further test execution.
+        /// </summary>
+        Terminate = 2
     }
 }
