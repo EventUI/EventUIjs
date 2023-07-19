75a930e25e3e0d2dbdc4d6e7d05181ecb964a75a
littleAndroidMan <littleAndroidMan@outlook.com>
2023-07-19 16:34:02 -0500

    Improved hash code test.

--- test/test_src/modules/core/hashCodes.js
+++ test/test_src/modules/core/hashCodes.js
@@ -17,6 +17,12 @@ var getNextCharacter = function(str, depth, chars)
 var result = getNextCharacter("", 2);
 
 var numResult = result.length;
+
+var numDictionaries = 0;
+var hashDictionaries = [];
+var keyCounter = 0;
+var maxKeys = 100000;
+
 var hashes = {};
 var collisions = {};
 
@@ -25,14 +31,29 @@ for (var x = 0; x < numResult; x++)
     var curStr = result[x];
     var curHash = $evui.getHashCode(curStr);
 
-    var existing = hashes[curHash];
+    var existing = null;
+    for (var y = 0; y < numDictionaries; y++)
+    {
+        existing = hashDictionaries[y][curHash];
+        if (existing != null) break;
+    }
+
     if (existing != null)
     {
         collisions[curHash] = existing;
         existing.push(curStr);
+        console.log("Collision! " + curStr + " = " + curHash);
     }
     else
     {
+        if (keyCounter === maxKeys)
+        {
+            numDictionaries = hashDictionaries.push(hashes);
+            hashes = {};
+            keyCounter = 0;
+        }
+
+        keyCounter++;
         hashes[curHash] = [curStr];
     }
 }
